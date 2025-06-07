2517a2fc2d1c42d4364aa3280297ba01fd347969
juuck14 <juuck14@gmail.com>
2025-06-08 00:08:25 +0900

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Header from "@/components/Header";
 import Search from "@/components/Search";
 import ParallelCoordinate from "@/components/ParallelCoordinate";
 import Compare from "@/components/Compare";
